a7c1fc122703a9e0f764de2856c916e7165e511f
oscilococcinum <gawlikbar@gmail.com>
2025-02-13 00:40:29 +0100

    cleans

--- flake.nix
+++ flake.nix
@@ -4,20 +4,18 @@
   };
 
   outputs = { nixpkgs, ... }@inputs: {
-    packages = builtins.listToAttrs (map (system:
-      {
-        name = system;
-        value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
+    packages = builtins.listToAttrs (map (system: {
+      name = system;
+      value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
 
-          cfmesh-cfdof = pkgs.callPackage (import ./cfmesh-cfdof) { openfoam = openfoam.${system}; };
-          cfmesh-cfdof-unstable = cfmesh-cfdof.override { version = "unstable"; };
+        cfmesh-cfdof = pkgs.callPackage (import ./cfmesh-cfdof) { openfoam = openfoam.${system}; };
+        cfmesh-cfdof-unstable = cfmesh-cfdof.override { version = "unstable"; };
 
-          hisa = pkgs.callPackage (import ./hisa) { openfoam = openfoam.${system}; };
-          hisa-unstable = hisa.override { version = "unstable"; };
+        hisa = pkgs.callPackage (import ./hisa) { openfoam = openfoam.${system}; };
+        hisa-unstable = hisa.override { version = "unstable"; };
 
-          openfoam = pkgs.callPackage (import ./openfoam-com) { };
-        };
-      }
-    )[ "x86_64-linux" "aarch64-linux" ]);
+        openfoam = pkgs.callPackage (import ./openfoam-com) { };
+      };
+    })[ "x86_64-linux" "aarch64-linux" ]);
   };
 }
